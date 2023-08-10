9b75510c1daeb91ea0db0ec4e88d3696eda024ae
Santiago Lopez <santiagomartinlopez05@gmail.com>
2023-08-09 21:02:04 -0300

    commit9

--- src/components/Certificados/Certificados.jsx
+++ src/components/Certificados/Certificados.jsx
@@ -8,26 +8,33 @@ const Certificados = () => {
           <h2 className='section__title'>CERTIFICADOS</h2>
         </div>
         <div className="row">
-          <div className="col-md-4 d-flex flex-column align-items-center cert">
+          <div className="col-md-3 d-flex flex-column align-items-center cert">
             <div className='m-1 cer'>
-              <img src="/img/Senasa.jpeg" alt="" />
-              <h3>Senasa</h3>
-              <p>lorem lorem lorem lorem lorem</p>
+              <img src="/img/sintacc.jpg" alt="sin tacc" />
+              <h3>Sin T.A.C.C.</h3>
+              <p>Pelayo se asegura de elaborar los productos sin Gluten.</p>
             </div>
           </div>
-          <div className="col-md-4 d-flex flex-column align-items-center cert">
+          <div className="col-md-3 d-flex flex-column align-items-center cert">
 
             <div className='m-1 cer'>
-              <img src="/img/Senasa.jpeg" alt="" />
-              <h3>Senasa</h3>
-              <p>lorem lorem lorem lorem lorem</p>
+              <img src="/img/Senasa.jpeg" alt="senasa" />
+              <h3>Servicio Nacional de Sanidad y Calidad Agroalimentaria</h3>
+              <p>Pelayo esta certificado por el Servicio Nacional de Sanidad y Calidad Agroalimentaria</p>
             </div>
           </div>
-          <div className="col-md-4 d-flex flex-column align-items-center cert">
+          <div className="col-md-3 d-flex flex-column align-items-center cert">
             <div className='m-1 cer'>
-              <img src="/img/Senasa.jpeg" alt="" />
-              <h3>Senasa</h3>
-              <p>lorem lorem lorem lorem lorem</p>
+              <img src="/img/premioapsal.jpg" alt="premio apsal" />
+              <h3>Premio Apsal 2018</h3>
+              <p>Pelayo recibio en 2018 el premio Apsal el cual reconoce la excelencia en productos y servicios de la Industria Productora de Alimentos.</p>
+            </div>
+          </div>
+          <div className="col-md-3 d-flex flex-column align-items-center cert">
+            <div className='m-1 cer'>
+              <img src="/img/acumar.png" alt="acumar" />
+              <h3>Acumar</h3>
+              <p>Pelayo esta certificado por la Autoridad de Cuenca Matanza Riachuelo</p>
             </div>
           </div>
         </div>
